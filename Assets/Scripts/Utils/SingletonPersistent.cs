10bcc9ecf6bf13b3dde144782147e2e123653728
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2024-03-09 13:15:30 -0300

    feat: adicionar pasta utils e script de singleton

---
+++ Assets/Scripts/Utils/SingletonPersistent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(-2)]
+public class MonoBehaviourSingletonPersistent<T> : MonoBehaviour
+    where T : Component
+{
+    public static T Instance { get; private set; }
+
+    public virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
