5b7a04e224c3496e743d0e6e4ef9ce927f5fed60
Tomás Pacheco <tomaspedron@gmail.com>
2024-03-21 17:22:06 -0300

    Adicionar gerador de código

--- Assets/Mecânicas/Porta com Código/CodigoFactory.cs
+++ Assets/Mecânicas/Porta com Código/CodigoFactory.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using Mecanicas.PortaComCodigo;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -8,6 +10,8 @@ public class CodigoFactory : NetworkBehaviour
 
     [SerializeField]
     private short[] digitos = new short[4];
+    List<CodigoSpawnItem> possibleItemToSpawn = new List<CodigoSpawnItem>();
+    List<CodigoSpawnItem> spawnedItems = new List<CodigoSpawnItem>();
 
     public override void OnNetworkSpawn()
     {
@@ -19,8 +23,9 @@ public class CodigoFactory : NetworkBehaviour
             {
                 digitos[i] = (short)random.Next(0, 10);
             }
-
             Debug.Log("Código gerado: " + digitos[0] + digitos[1] + digitos[2] + digitos[3]);
+            possibleItemToSpawn.AddRange(FindObjectsOfType<CodigoSpawnItem>());
+            PickFourObjectsAtRandom();
         }
         else
         {
@@ -46,7 +51,36 @@ public class CodigoFactory : NetworkBehaviour
     {
         return digitos;
     }
+    private void PickFourObjectsAtRandom()
+    {
+        if(possibleItemToSpawn.Count == 0)
+        {
+            Debug.LogError("Não há objetos para spawnar os itens.");
+            return;
+        }
+        if (possibleItemToSpawn.Count < digitos.Length)
+        {
+            Debug.LogWarning("Não há objetos suficientes para spawnar os itens.");
+            int index = Random.Range(0, possibleItemToSpawn.Count);
+            CodigoSpawnItem item = possibleItemToSpawn[index];
+            item.setText(0, digitos[0]);
+            item.setText(1, digitos[1]);
+            item.setText(2, digitos[2]);
+            item.setText(3, digitos[3]);
 
+            spawnedItems.AddRange(new []{item, item, item, item});
+            possibleItemToSpawn.RemoveAt(index);
+            return;
+        }
+        for (int i = 0; i < digitos.Length; i++)
+        {
+            int index = Random.Range(0, possibleItemToSpawn.Count);
+            CodigoSpawnItem item = possibleItemToSpawn[index];
+            item.setText(i, digitos[i]);
+            spawnedItems.Add(item);
+            possibleItemToSpawn.RemoveAt(index);
+        }
+    }
     public bool CheckCodigo(short[] codigo)
     {
         //essa linha evita que o usuário envie um código vazio
