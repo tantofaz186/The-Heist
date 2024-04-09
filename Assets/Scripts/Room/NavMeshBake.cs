7e3e1b186808f5c70a875dc1b367bbabcf6effdf
pmarqueslopes <pmarqueslopes02@gmail.com>
2024-04-08 21:35:27 -0300

    Bake NavMesh

---
+++ Assets/Scripts/Room/NavMeshBake.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEditor.AI;
+
+public class NavMeshBake : MonoBehaviour
+{   
+   public void Bake()
+    {   
+        NavMeshBuilder.BuildNavMesh();
+        Debug.Log("Baked");
+    }
+}
