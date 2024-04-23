3da76892894cbbff1e8d07542a5e767dfa64ecf0
Tomás Pedron Turchi Pacheco <42844869+tantofaz186@users.noreply.github.com>
2024-04-22 22:14:31 -0300

    fix: fov resetting raycast list

--- Assets/PreFabs/Enemy/Enemy.cs
+++ Assets/PreFabs/Enemy/Enemy.cs
@@ -1,111 +1,92 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
-
+[RequireComponent(typeof(FOV))]
 public class Enemy : NetworkBehaviour
 {
     public NavMeshAgent agent;
-   
+
     public Animator anim;
-    public Rigidbody rb;
-
-    public Transform playerPos;
-
-    public LayerMask whatIsGround, whatIsPlayer;
-
+    public bool gameStarted = true;
     public Vector3 walkPoint;
     private bool walkPointSet;
     public float walkPointRange;
-
+    public float attackRange;
     public float timeAttack;
     private bool attacked;
 
     public GameObject playerFound;
 
-    public float sightRange, attackRange;
-   
-
-    
-    
-    
     public FOV fov;
-    
+
     private void Awake()
     {
-       
-        rb = GetComponent<Rigidbody>();
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponentInChildren<Animator>();
-       // fov = GetComponent<FOV>();
+        fov = GetComponent<FOV>();
     }
-    
+
     GameObject FindPlayer(FOV sensor)
     {
-        if(sensor.Objects.Count > 0)
+        if (sensor.Objects.Count > 0)
         {
             return sensor.Objects.First();
         }
 
         return null;
     }
-    
 
-    
-    void Update()
-    {   
-        Collider[] col=  Physics.OverlapSphere(transform.position, attackRange, whatIsPlayer);
-        if (col.Length > 0)
-        {
-            Attack(col[0].transform);
-            return;
-        }
-        Collider[] col2=  Physics.OverlapSphere(transform.position, sightRange, whatIsPlayer);
-        if (col2.Length > 0)
-        {
-            Chase(col2[0].transform);
-            return;
-        }
-        /*if(fov.Objects.Count > 0)
-        {
-            playerFound = FindPlayer(fov);
-            if (playerFound)
-            {
-               Chase(playerFound.transform);
-               return;
-            }
-        }*/
-            Patrol();
-        
-        
-        
-            
-        
-        
+    private void Start()
+    {
+        StartCoroutine(FovScan());
     }
-    
-    
+
+    private IEnumerator FovScan()
+    {
+        while (gameStarted)
+        {
+            fov.Scan();
+            if (fov.Objects.Count > 0)
+            {
+                playerFound = FindPlayer(fov);
+                if (playerFound)
+                {
+                    if (Vector3.Distance(transform.position, playerFound.transform.position) < attackRange)
+                    {
+                        Attack(playerFound.transform);
+                    }
+                    else
+                    {
+                        Chase(playerFound.transform);
+                    }
+                }
+                else
+                {
+                    Patrol();
+                }
+
+                yield return new WaitForSeconds(fov.scanInterval);
+            }
+        }
+    }
 
     void Patrol()
-    {   
-        
-       if(!walkPointSet)SearchWalk();
-       if (walkPointSet) agent.SetDestination(walkPoint);
-       agent.speed = 1f;
-        anim.SetBool("walk",true);
-        anim.SetBool("run",false);
-       Vector3 distance = transform.position - walkPoint;
+    {
+        if (!walkPointSet) SearchWalk();
+        if (walkPointSet) agent.SetDestination(walkPoint);
+        agent.speed = 1f;
+        anim.SetBool("walk", true);
+        anim.SetBool("run", false);
+        Vector3 distance = transform.position - walkPoint;
 
-       if (distance.magnitude < 1f)
-       {
-           walkPointSet = false;
-           
-       }
+        if (distance.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
     }
 
     void SearchWalk()
@@ -113,7 +94,8 @@ public class Enemy : NetworkBehaviour
         float randomz = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomz);
+        var position = transform.position;
+        walkPoint = new Vector3(position.x + randomX, position.y, position.z + randomz);
 
         if (Physics.Raycast(walkPoint, -transform.up, 2f))
         {
@@ -121,24 +103,24 @@ public class Enemy : NetworkBehaviour
         }
     }
 
-    void Chase(Transform playerPos)
-    {   
-        agent.SetDestination(playerPos.transform.position);
+    void Chase(Transform _playerPos)
+    {
+        agent.SetDestination(_playerPos.transform.position);
         agent.speed = 3f;
-        anim.SetBool("run",true);
-        anim.SetBool("walk",false);
-        
+        anim.SetBool("run", true);
+        anim.SetBool("walk", false);
     }
 
-    void Attack(Transform playerPos)
+    void Attack(Transform _playerPos)
     {
-        //agent.SetDestination(playerPos.position);
-        transform.LookAt(playerPos.position);
+        var position = _playerPos.position;
+        agent.SetDestination(position);
+        transform.LookAt(position);
         if (!attacked)
         {
             anim.SetTrigger("attack");
             attacked = true;
-            Invoke(nameof(ResetAttack),timeAttack);
+            Invoke(nameof(ResetAttack), timeAttack);
         }
     }
 
--- Assets/PreFabs/Enemy/FOV.cs
+++ Assets/PreFabs/Enemy/FOV.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+
 [ExecuteInEditMode]
 public class FOV : MonoBehaviour
 {
@@ -10,49 +11,32 @@ public class FOV : MonoBehaviour
     public float viewHeight;
     public LayerMask playerMask;
     public LayerMask obstacleMask;
-    
 
     public Color meshColor = Color.red;
 
     private Mesh mesh;
 
     private Collider[] colliders = new Collider[50];
-    private float scanInterval;
+    public float scanInterval;
     private float scanTimer;
     public int scanFrequency = 30;
     private int count;
 
-    public List<GameObject> Objects
-    {
-        get
-        {
-            objects.RemoveAll(obj=>!obj);
-            return objects;
-        }
-    }
-    [SerializeField]private List<GameObject> objects = new();
+    public List<GameObject> Objects => objects;
 
-    
-    private void Start()
+    [SerializeField]
+    private List<GameObject> objects = new();
+
+    private void Awake()
     {
         scanInterval = 1.0f / scanFrequency;
     }
 
-    private void Update()
+    public void Scan()
     {
-        scanTimer-=Time.deltaTime;
-        if (scanTimer < 0)
-        {
-            scanTimer += scanInterval;
-            Scan();
-        }
-    }
-
-    private void Scan()
-    {
-        count =Physics.OverlapSphereNonAlloc(transform.position,viewRadius,colliders,playerMask,QueryTriggerInteraction.Collide);
         objects.Clear();
-        for (int i = 0; i <count; i++)
+        count = Physics.OverlapSphereNonAlloc(transform.position, viewRadius, colliders, playerMask, QueryTriggerInteraction.Collide);
+        for (int i = 0; i < count; i++)
         {
             GameObject obj = colliders[i].gameObject;
             if (InSight(obj))
@@ -60,11 +44,10 @@ public class FOV : MonoBehaviour
                 objects.Add(obj);
             }
         }
-        
     }
 
     public bool InSight(GameObject obj)
-    {   
+    {
         Vector3 origin = transform.position;
         Vector3 dest = obj.transform.position;
         Vector3 direction = dest - origin;
@@ -72,37 +55,34 @@ public class FOV : MonoBehaviour
         {
             return false;
         }
-        direction.y=0;
+
+        direction.y = 0;
         float deltaAngle = Vector3.Angle(direction, transform.forward);
         if (deltaAngle > viewAngle)
         {
             return false;
         }
-        
-        origin.y += viewHeight/2;
+
+        origin.y += viewHeight / 2;
         dest.y = origin.y;
-        if (Physics.Linecast(origin,dest, obstacleMask))
-        {
-            return false;
-        }
-        
-        return true;
+        return !Physics.Linecast(origin, dest, obstacleMask);
     }
 
-    /*Mesh CreateWedgeMesh()  //Criar triangulo
+    #if UNITY_EDITOR
+
+    Mesh CreateWedgeMesh() //Criar triangulo
     {
-        
         Mesh mesh = new Mesh();
-        
+
         int segments = 10;
-        int numTriangles = (segments*4)+2+2;
+        int numTriangles = (segments * 4) + 2 + 2;
         int numVertices = numTriangles * 3;
         Vector3[] vertices = new Vector3[numVertices];
-        int [] triangles = new int[numVertices];
+        int[] triangles = new int[numVertices];
         Vector3 bottomCenter = Vector3.zero;
-        Vector3 bottomLeft = Quaternion.Euler(0,-viewAngle,0)*Vector3.forward*viewRadius;
-        Vector3 bottomRight = Quaternion.Euler(0,viewAngle,0)*Vector3.forward*viewRadius;
-        
+        Vector3 bottomLeft = Quaternion.Euler(0, -viewAngle, 0) * Vector3.forward * viewRadius;
+        Vector3 bottomRight = Quaternion.Euler(0, viewAngle, 0) * Vector3.forward * viewRadius;
+
         Vector3 topCenter = bottomCenter + Vector3.up * viewHeight;
         Vector3 topLeft = bottomLeft + Vector3.up * viewHeight;
         Vector3 topRight = bottomRight + Vector3.up * viewHeight;
@@ -112,7 +92,7 @@ public class FOV : MonoBehaviour
         vertices[vert++] = bottomCenter;
         vertices[vert++] = bottomLeft;
         vertices[vert++] = topLeft;
-        
+
         vertices[vert++] = topLeft;
         vertices[vert++] = topCenter;
         vertices[vert++] = bottomCenter;
@@ -120,49 +100,48 @@ public class FOV : MonoBehaviour
         vertices[vert++] = bottomCenter;
         vertices[vert++] = topCenter;
         vertices[vert++] = topRight;
-        
+
         vertices[vert++] = topRight;
         vertices[vert++] = bottomRight;
         vertices[vert++] = bottomCenter;
-        
+
         float currentAngle = -viewAngle;
-        float deltaAngle = (viewAngle*2)/segments;
+        float deltaAngle = (viewAngle * 2) / segments;
         for (int i = 0; i < segments; i++)
         {
-            bottomLeft = Quaternion.Euler(0,currentAngle,0)*Vector3.forward*viewRadius;
-            bottomRight = Quaternion.Euler(0,currentAngle+deltaAngle,0)*Vector3.forward*viewRadius;
-            
+            bottomLeft = Quaternion.Euler(0, currentAngle, 0) * Vector3.forward * viewRadius;
+            bottomRight = Quaternion.Euler(0, currentAngle + deltaAngle, 0) * Vector3.forward * viewRadius;
+
             topRight = bottomRight + Vector3.up * viewHeight;
             topLeft = bottomLeft + Vector3.up * viewHeight;
-            
+
             //far side
-                        vertices[vert++] = bottomLeft;
-                        vertices[vert++] = bottomRight;
-                        vertices[vert++] = topRight;
-                    
-                        vertices[vert++] = topRight;
-                        vertices[vert++] = topLeft;
-                        vertices[vert++] = bottomLeft;
-                        //top side
-                        vertices[vert++] = topCenter;
-                        vertices[vert++] = topLeft;
-                        vertices[vert++] = topRight;
-                    
-                        //bottom side
-                        vertices[vert++] = bottomCenter;
-                        vertices[vert++] = bottomRight;
-                        vertices[vert++] = bottomLeft;
-            
+            vertices[vert++] = bottomLeft;
+            vertices[vert++] = bottomRight;
+            vertices[vert++] = topRight;
+
+            vertices[vert++] = topRight;
+            vertices[vert++] = topLeft;
+            vertices[vert++] = bottomLeft;
+            //top side
+            vertices[vert++] = topCenter;
+            vertices[vert++] = topLeft;
+            vertices[vert++] = topRight;
+
+            //bottom side
+            vertices[vert++] = bottomCenter;
+            vertices[vert++] = bottomRight;
+            vertices[vert++] = bottomLeft;
+
             currentAngle += deltaAngle;
-            
         }
-        
-       
-        
-        for(int i=0;i<vertices.Length;i++)
+
+
+        for (int i = 0; i < vertices.Length; i++)
         {
             triangles[i] = i;
         }
+
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
@@ -173,7 +152,6 @@ public class FOV : MonoBehaviour
     {
         mesh = CreateWedgeMesh();
         scanInterval = 1.0f / scanFrequency;
-
     }
 
     private void OnDrawGizmos()
@@ -183,12 +161,13 @@ public class FOV : MonoBehaviour
             Gizmos.color = meshColor;
             Gizmos.DrawMesh(mesh, transform.position, transform.rotation);
         }
-        
-        
+
+
         Gizmos.color = Color.green;
         foreach (var obj in Objects)
         {
             Gizmos.DrawSphere(obj.transform.position, 0.2f);
         }
-    }*/
+    }
+    #endif
 }
