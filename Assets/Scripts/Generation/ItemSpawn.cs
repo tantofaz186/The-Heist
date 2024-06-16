80bc032be0e3d2cc02c9b557899bc0a058d25af4
pmarqueslopes <pmarqueslopes02@gmail.com>
2024-06-16 17:16:08 -0300

    Reliquias

--- Assets/Scripts/Generation/ItemSpawn.cs
+++ Assets/Scripts/Generation/ItemSpawn.cs
@@ -63,7 +63,7 @@ public class ItemSpawn : NetworkBehaviour
       bool inList = false;
       if (relicsCheck.Count <= 0)
       {
-         relicsCheck = new List<int>(){0,1};
+         relicsCheck = new List<int>(){0,1,2,3};
           
       }
            
