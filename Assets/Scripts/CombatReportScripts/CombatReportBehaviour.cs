c7e72d1e9e3318c2fcfa440fec5326479bcd73db
Tomás Pacheco <tomaspedron@gmail.com>
2024-09-24 21:36:20 -0300

    Update CombatReportBehaviour.cs

--- Assets/Scripts/CombatReportScripts/CombatReportBehaviour.cs
+++ Assets/Scripts/CombatReportScripts/CombatReportBehaviour.cs
@@ -44,30 +44,6 @@ namespace CombatReportScripts
                 vezesAtacado = 0,
             };
         }
-        
-        private IEnumerator Start()
-        {
-            yield return new WaitUntil(() => PlayerActions.Instance.ready);
-            PlayerActions.Instance.PlayerInputActions.Player.Item1.performed += Item1Onperformed;
-            PlayerActions.Instance.PlayerInputActions.Player.Item2.performed += Item2Onperformed;
-            PlayerActions.Instance.PlayerInputActions.Player.Item3.performed += Item3Onperformed;
-        }
-
-        private void Item1Onperformed(InputAction.CallbackContext obj)
-        {
-            combatReportData.dinheiroRecebido += 50;
-        }
-
-        private void Item2Onperformed(InputAction.CallbackContext obj)
-        {
-            combatReportData.itensUsados++;
-        }
-
-        private void Item3Onperformed(InputAction.CallbackContext obj)
-        {
-            combatReportData.itensColetados++;
-        }
-
         public void Save()
         {
             SaveSystem.SaveCombatReport(combatReportData);
@@ -75,12 +51,6 @@ namespace CombatReportScripts
         private void OnDisable()
         {
             Save();
-            if (PlayerActions.Instance != null)
-            {
-                PlayerActions.Instance.PlayerInputActions.Player.Item1.performed -= Item1Onperformed;
-                PlayerActions.Instance.PlayerInputActions.Player.Item2.performed -= Item2Onperformed;
-                PlayerActions.Instance.PlayerInputActions.Player.Item3.performed -= Item3Onperformed;
-            }
         }
 
     }
