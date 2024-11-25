0cce0f43ec8871f913c800487ac0ee736fa85bba
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2024-11-24 21:18:04 -0300

    Update ItemSelect.cs

--- Assets/Scripts/Inventory/ItemSelect.cs
+++ Assets/Scripts/Inventory/ItemSelect.cs
@@ -1,11 +1,23 @@
 using Unity.Netcode;
 using UnityEngine.InputSystem;
 
-public class ItemSelect : Singleton<ItemSelect>, IUseAction
+public class ItemSelect : NetworkBehaviour, IUseAction
 {
     public BaseItem itemInHand;
-
+    public static ItemSelect Instance { get; private set; }
     public int currentItemIndex { get; private set; }
+
+    private void Awake()
+    {
+        if (IsOwner)
+        {
+            Instance = this;
+        }
+        else
+        {
+            enabled = false;
+        }
+    }
 
     public void setActions()
     {
