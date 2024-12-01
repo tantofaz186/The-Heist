828bca44bf09d215aedf2b81629a7d6295eb57d5
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2024-12-01 15:40:19 -0300

    fix options menu

--- Assets/Scripts/Itens/NightVision.cs
+++ Assets/Scripts/Itens/NightVision.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using Unity.Netcode;
+
 public class NightVision : BaseItem
 {
     bool isNightVisionActive = false;
     bool coolDown = false;
     public Camera cam;
     private Volume volume;
-    [SerializeField] VolumeProfile nightVisionProfile;
-    [SerializeField] VolumeProfile defaultProfile;
-    
+
+    [SerializeField]
+    VolumeProfile nightVisionProfile;
+
+    [SerializeField]
+    VolumeProfile defaultProfile;
+
     public override void UseItem()
     {
         if (!coolDown)
@@ -24,8 +29,9 @@ public class NightVision : BaseItem
             {
                 TurnOnNightVision();
             }
+
             coolDown = true;
-            Invoke(nameof(CoolDown),1.5f);
+            Invoke(nameof(CoolDown), 1.5f);
         }
     }
 
@@ -35,20 +41,23 @@ public class NightVision : BaseItem
         volume = GameObject.FindGameObjectWithTag("GlobalVolume").GetComponent<Volume>();
     }
 
-    
     void TurnOnNightVision()
-    {   
-        if(cam == null)return;
-        
+    {
+        if (cam == null) return;
+
         isNightVisionActive = true;
         volume.profile = nightVisionProfile;
-        cam.cullingMask = LayerMask.GetMask("Default","TransparentFX","Ignore Raycast","Cam","Water","UI","Ground","PlayerLayer", "Keypad","Door","PlayerDontSee","Obstacle","VaultRoom","Roof","Bullet","PostProcessing","Enemy","NightVision");
+        cam.cullingMask = LayerMask.GetMask("Default", "TransparentFX", "Ignore Raycast", "Cam", "Water", "UI", "Ground", "PlayerLayer",
+            "Keypad", "Door", "PlayerDontSee", "Obstacle", "VaultRoom", "Roof", "Bullet", "PostProcessing", "Enemy", "NightVision");
     }
+
     void TurnOffNightVision()
-    {   if(cam == null)return;
+    {
+        if (cam == null) return;
         isNightVisionActive = false;
         volume.profile = defaultProfile;
-        cam.cullingMask = LayerMask.GetMask("Default","TransparentFX","Ignore Raycast","Cam","Water","UI","Ground","PlayerLayer", "Keypad","Door","Item","PlayerDontSee","Obstacle","VaultRoom","Roof","Bullet","PostProcessing","Enemy");
+        cam.cullingMask = LayerMask.GetMask("Default", "TransparentFX", "Ignore Raycast", "Cam", "Water", "UI", "Ground", "PlayerLayer",
+            "Keypad", "Door", "Item", "PlayerDontSee", "Obstacle", "VaultRoom", "Roof", "Bullet", "PostProcessing", "Enemy");
     }
 
     void CoolDown()
@@ -59,16 +68,12 @@ public class NightVision : BaseItem
     public override void OnPick(ulong playerId)
     {
         base.OnPick(playerId);
-        
-       cam =NetworkManager.SpawnManager.GetPlayerNetworkObject(playerId).GetComponent<PlayerActions>()._camera;
-       
+
+        cam = NetworkManager.SpawnManager.GetPlayerNetworkObject(playerId).GetComponent<PlayerActions>()._camera;
     }
-    
+
     public override void OnDrop()
-    {           
-            TurnOffNightVision();
+    {
+        TurnOffNightVision();
     }
-    
-   
-    
 }
--- Assets/Scripts/UI/OptionsMenu.cs
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -94,7 +94,7 @@ public class OptionsMenu : MonoBehaviour
 
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height + " : " + resolutions[i].refreshRateRatio;
+            string option = $"{resolutions[i].width} x  {resolutions[i].height} : {resolutions[i].refreshRateRatio.value:F0}";
             options.Add(option);
 
             if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
