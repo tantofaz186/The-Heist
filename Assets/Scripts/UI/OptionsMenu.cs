d1b4207c86a7bb701fe44ecfb63f3b4de28a15c7
Tomás Pedron Turchi Pacheco <tomaspedron@gmail.com>
2024-12-01 15:21:36 -0300

    Update OptionsMenu.cs

--- Assets/Scripts/UI/OptionsMenu.cs
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -5,15 +5,26 @@ using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using UnityEngine.Audio;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
-{   [SerializeField] private GameObject OptionsMenuPanel;
-    [SerializeField] private GameObject GraphicsPanel;
-    [SerializeField] private GameObject SoundPanel;
-    [SerializeField] private GameObject ControlsPanel;
-    [SerializeField] private GameObject QuitPanel;
-    
+{
+    [SerializeField]
+    private GameObject OptionsMenuPanel;
+
+    [SerializeField]
+    private GameObject GraphicsPanel;
+
+    [SerializeField]
+    private GameObject SoundPanel;
+
+    [SerializeField]
+    private GameObject ControlsPanel;
+
+    [SerializeField]
+    private GameObject QuitPanel;
+
     private bool menuOpen = false;
     public Slider brightnessSlider;
     public Slider MasterVolumeSlider;
@@ -22,23 +33,19 @@ public class OptionsMenu : MonoBehaviour
     public AudioMixer audioMixer;
     public TMP_Dropdown resolutionDropdown;
     private Resolution[] resolutions;
-    
-    
+
     public static OptionsMenu instance;
-   
-    
-    
+
     PlayerInputActions inputActions;
 
     void Start()
-    {   
+    {
         GetResolutions();
-        MasterVolumeSlider.value = audioMixer.GetFloat("MasterVolume", out float value) ? Mathf.Pow(10,value/20) : 0;
-        MusicSlider.value = audioMixer.GetFloat("MusicVolume", out float val) ? Mathf.Pow(10,val/20) : 0;
-        SfxSlider.value = audioMixer.GetFloat("SfxVolume", out float valu) ? Mathf.Pow(10,valu/20) : 0;
+        MasterVolumeSlider.value = audioMixer.GetFloat("MasterVolume", out float value) ? Mathf.Pow(10, value / 20) : 0;
+        MusicSlider.value = audioMixer.GetFloat("MusicVolume", out float val) ? Mathf.Pow(10, val / 20) : 0;
+        SfxSlider.value = audioMixer.GetFloat("SfxVolume", out float valu) ? Mathf.Pow(10, valu / 20) : 0;
         brightnessSlider.value = RenderSettings.ambientIntensity;
         OptionsMenuPanel.SetActive(false);
-        
     }
 
     private void OnEnable()
@@ -52,32 +59,31 @@ public class OptionsMenu : MonoBehaviour
     {
         inputActions.Player.Menu.performed -= OnPause;
         inputActions.Disable();
-        
     }
 
     private void OnPause(InputAction.CallbackContext obj)
     {
         OnPause();
     }
+
     public void OnPause()
-         {
-             if (menuOpen)
-             {
-                 OptionsMenuPanel.SetActive(false);
-                 menuOpen = false;
-                 if (SceneManager.GetActiveScene().name == "GameScene")
-                 {
-                     Cursor.lockState = CursorLockMode.Locked;
-                 }
-                 
-             }
-             else
-             {
-                 OptionsMenuPanel.SetActive(true);
-                 menuOpen = true;
-                 Cursor.lockState = CursorLockMode.None;
-             }
-         }
+    {
+        if (menuOpen)
+        {
+            OptionsMenuPanel.SetActive(false);
+            menuOpen = false;
+            if (SceneManager.GetActiveScene().name == "GameScene")
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+        else
+        {
+            OptionsMenuPanel.SetActive(true);
+            menuOpen = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
 
     void GetResolutions()
     {
@@ -85,22 +91,23 @@ public class OptionsMenu : MonoBehaviour
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
         int currentResolutionIndex = 0;
-        
+
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = resolutions[i].width + " x " + resolutions[i].height + " : " + resolutions[i].refreshRateRatio;
             options.Add(option);
-            
+
             if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
             {
                 currentResolutionIndex = i;
             }
         }
+
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
     }
-    
+
     public void SetResolution(int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];
@@ -111,25 +118,27 @@ public class OptionsMenu : MonoBehaviour
     {
         Screen.fullScreen = isFullscreen;
     }
-    
+
     public void Brightness()
     {
         RenderSettings.ambientIntensity = brightnessSlider.value;
     }
-    
+
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
-    
+
     public void SetMasterVolume()
     {
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(MasterVolumeSlider.value) * 20);
     }
+
     public void SetMusicVolume()
     {
         audioMixer.SetFloat("MusicVolume", Mathf.Log10(MusicSlider.value) * 20);
     }
+
     public void SetSfxVolume()
     {
         audioMixer.SetFloat("SfxVolume", Mathf.Log10(SfxSlider.value) * 20);
@@ -142,21 +151,24 @@ public class OptionsMenu : MonoBehaviour
         ControlsPanel.SetActive(false);
         QuitPanel.SetActive(false);
     }
-   public void Graphics()
+
+    public void Graphics()
     {
         GraphicsPanel.SetActive(true);
         SoundPanel.SetActive(false);
         ControlsPanel.SetActive(false);
         QuitPanel.SetActive(false);
     }
-   public void Controls()
+
+    public void Controls()
     {
         GraphicsPanel.SetActive(false);
         SoundPanel.SetActive(false);
         ControlsPanel.SetActive(true);
         QuitPanel.SetActive(false);
-    }   
-   public void QuitMenu()
+    }
+
+    public void QuitMenu()
     {
         GraphicsPanel.SetActive(false);
         SoundPanel.SetActive(false);
@@ -168,8 +180,4 @@ public class OptionsMenu : MonoBehaviour
     {
         Application.Quit();
     }
-
-  
-
-    
 }
